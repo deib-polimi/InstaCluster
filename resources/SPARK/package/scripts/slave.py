bab642f8ff6b54864cf03409eeaa49f4b9cc0c16
Srdjan Krstic <krledmno1@gmail.com>
2015-03-09 19:14:26 +0100

    slave py fix

--- resources/SPARK/package/scripts/slave.py
+++ resources/SPARK/package/scripts/slave.py
@@ -3,16 +3,16 @@ from resource_management import *
 import subprocess
 
 class Slave(Script):
-  def install(self, env):
+  def install(self, env):
     subprocess.call("/home/ubuntu/HDP-amazon-scripts/script/spark/spark_worker.sh", shell=True)
-  def stop(self, env):
+  def stop(self, env):
     subprocess.call("/home/ubuntu/HDP-amazon-scripts/script/spark/spark_worker_stop.sh", shell=True)
-  def start(self, env):
+  def start(self, env):
     subprocess.call("/home/ubuntu/HDP-amazon-scripts/script/spark/spark_worker_start.sh", shell=True)
-    
-  def status(self, env):
-    print 'Status of the Sample Srv Slave';
-  def configure(self, env):
-    print 'Configure the Sample Srv Slave';
+   
+  def status(self, env):
+    print 'Status of the Sample Srv Slave';
+  def configure(self, env):
+    print 'Configure the Sample Srv Slave';
 if __name__ == "__main__":
-  Slave().execute()
+   Slave().execute()
